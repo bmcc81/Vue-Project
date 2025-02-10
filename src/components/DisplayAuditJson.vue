cdb5342cb4ccbc2433dba436d0126f44b1d87589
bmcc81 <bmcc81@gmail.com>
2025-02-10 13:38:39 -0500

    Displaying the Json vulnerabilities in string on scren

---
+++ src/components/DisplayAuditJson.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+import json from '../../audit.json'
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      auditJson: {},
+    }
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+    fetchAudit() {
+      this.auditJson = json
+    },
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    this.fetchAudit()
+  },
+}
+</script>
+
+<template>
+  <div class="greetings">{{ this.auditJson }}</div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
